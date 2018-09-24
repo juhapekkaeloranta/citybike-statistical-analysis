d6d3aa8bf99b47fb96abea68e569824c1abaac31
Mikko Kotola <mikko.kotola@iki.fi>
2018-09-24 18:48:14 +0300

    get-weather-forecast now fetches forecast and writes csv to folder prediction.

--- python-src/get-weather-forecast.py
+++ python-src/get-weather-forecast.py
@@ -1,5 +1,6 @@
+import numpy as np
+import pandas as pd
 import sys, os
-#import urllib
 from xml.etree import ElementTree as ET
 import urllib.request
 import datetime
@@ -8,13 +9,13 @@ sys.path.append(os.path.realpath('..'))
 
 WEATHERLOCATION = 'kaisaniemi,helsinki'
 FMIAPIKEYSOURCE = 'env'
-#WEATHERFORECASTOUTXMLFILE = '/prediction/weather' + datetime.datetime.now().replace(microsecond=0).isoformat() + '.xml'
-WEATHERFORECASTOUTXMLFILE = 'prediction/weatherforecast.xml'
+WEATHERFORECASTOUTCSVFILE = 'prediction/weatherforecast-HelsinkiKaisaniemi-' + datetime.datetime.now().replace(microsecond=0).isoformat() + '.csv'
 
 def getFmiApiKey(f):
     root = ET.parse(f).getroot()
     return root.find('fmi-api-key').text
 
+# Fetch the weather forecast for Helsinki Kaisaniemi from FMI API
 requestURL = 'http://data.fmi.fi/fmi-apikey/' \
            + getFmiApiKey(FMIAPIKEYSOURCE) + '/' \
            + 'wfs?request=getFeature&storedquery_id=' \
@@ -23,30 +24,19 @@ requestURL = 'http://data.fmi.fi/fmi-apikey/' \
 
 req = urllib.request.Request(requestURL)
 response = urllib.request.urlopen(req)
+
+# Parse the temperature and rain forecasts from the FMI XML file
 tree = ET.parse(response)
 root = tree.getroot()
 
-# Up to here OK. But parsing the wanted parts not working yet.
-
-# This doesn't work
-#temperatureElement = root.find('.//MeasurementTimeseries[@id="{http://www.opengis.net/gml/3.2}mts-1-1-Temperature"]')
-
-print(ET.tostring(root, encoding='utf8').decode('utf8'))
-
-#temperatureSeries = {}
-#rainAmountSeries = {}
-print('Individual elements: ')
-temperatureElements = root.findall('.//{http://www.opengis.net/waterml/2.0}MeasurementTimeseries')
-for el in temperatureElements:
+measurementElements = root.findall('.//{http://www.opengis.net/waterml/2.0}MeasurementTimeseries')
+for el in measurementElements:
     if (el.attrib['{http://www.opengis.net/gml/3.2}id'] == 'mts-1-1-Temperature'):
         temperatureSeries = el
-        #print(ET.tostring(el, encoding='utf8').decode('utf8'))
     elif (el.attrib['{http://www.opengis.net/gml/3.2}id'] == 'mts-1-1-PrecipitationAmount'):
         rainAmountSeries = el
-        #print(ET.tostring(el, encoding='utf8').decode('utf8'))
 
 
-# This parses all MeasurementTimeseries, but I have been unable to filter the correct series by e.g. '{http://www.opengis.net/gml/3.2}id'] == 'mts-1-1-Temperature'
 def parser(item1,item2):
     return item1.text,item2.text
 
@@ -57,10 +47,17 @@ def parse_one_series(series):
     
 
 temperatureData = zip(*(parse_one_series(temperatureSeries)))
-print(list(temperatureData))
+temperatureDataList = list(list(temperatureData))
 
 rainAmountData = zip(*(parse_one_series(rainAmountSeries)))
-print(list(rainAmountData))
+rainDataList = list(list(rainAmountData))
 
-#temperatureElement.write(WEATHERFORECASTOUTXMLFILE)
-#root.write(WEATHERFORECASTOUTXMLFILE)
+# Put the data into pandas a dataframe
+df_temperature = pd.DataFrame(temperatureDataList).T
+df_weatherPred = pd.DataFrame(rainDataList).T
+
+df_weatherPred[2] = df_temperature[1]
+df_weatherPred.columns = ['Time','RainAmountPred','TemperaturePred']
+
+# Write to csv
+df_weatherPred.to_csv(WEATHERFORECASTOUTCSVFILE, index=False)
