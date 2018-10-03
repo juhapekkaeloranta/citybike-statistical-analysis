def61b6e5b748cb37cdefcc1da4a5c23d1088f34
Mikko Kotola <mikko.kotola@iki.fi>
2018-10-03 20:07:46 +0300

    Added basic http server serving dummy json data.

---
+++ server/server.py
@@ -0,0 +1,45 @@
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 3001
+
+class ReqHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        paths = {
+            '/prediction': {'status': 200}
+        }
+
+        if self.path in paths:
+            self.respond(paths[self.path])
+        else:
+            self.respond({'status': 500})
+
+    def handle_http(self, status_code, path):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        content = json.dumps({"1": 17, "2": 20})
+        return bytes(content, 'UTF-8')
+
+    def respond(self, opts):
+        response = self.handle_http(opts['status'], self.path)
+        self.wfile.write(response)
+       
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), ReqHandler)
+    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
