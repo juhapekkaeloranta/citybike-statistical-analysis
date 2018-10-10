c3370eb5062673ce49e00bcb3505ca0a486e7970
Mikko Kotola <mikko.kotola@iki.fi>
2018-10-10 17:46:34 +0300

    Added support for reading in multiple weather files.

--- python-src/read_history_data.py
+++ python-src/read_history_data.py
@@ -3,24 +3,33 @@ import sys, os
 import numpy as np
 import datetime
 
-
 AVAIL_DATA_HISTORY_FILES = ['data/02-hourly-avg/bikeAvailability-2017-06-hourly-avg-per-station.csv',
 'data/02-hourly-avg/bikeAvailability-2017-08-hourly-avg-per-station.csv', 
 'data/02-hourly-avg/bikeAvailability-2017-09-hourly-avg-per-station.csv']
-WEATHER_DATA_HISTORY_FILE = 'weather-data/fmi-weatherdata-Helsinki-Kaisaniemi-2017.csv'
+WEATHER_DATA_HISTORY_FILES = ['weather-data/fmi-weatherdata-Helsinki-Kaisaniemi-2016.csv',
+'weather-data/fmi-weatherdata-Helsinki-Kaisaniemi-2017.csv', 
+'weather-data/fmi-weatherdata-Helsinki-Kaisaniemi-2018.csv']
 
 def readData():
     print('\n  Reading in history data for model creation...')
     # Read in data
     df_from_each_bikefile = (pd.read_csv(f) for f in AVAIL_DATA_HISTORY_FILES)
-    bikeData   = pd.concat(df_from_each_bikefile, ignore_index=True)
+    bikeData = pd.concat(df_from_each_bikefile, ignore_index=True)
 
-    weatherData = pd.read_csv(WEATHER_DATA_HISTORY_FILE, sep=",")
+    df_from_each_weatherfile = (pd.read_csv(f, sep=",") for f in WEATHER_DATA_HISTORY_FILES)
+    weatherData = pd.concat(df_from_each_weatherfile, ignore_index=True)
+
+    #weatherData = pd.read_csv(WEATHER_DATA_HISTORY_FILES, sep=",")
     weatherData.rename(columns={'Vuosi': 'Year', 'Kk': 'Month', 'Pv': 'Day', 'Klo': 'HourMin', 'Aikavyöhyke': 'Timezone', 'Sateen intensiteetti (mm/h)': 'rainIntensity_mmh', 'Ilman lämpötila (degC)': 'temperature_c'}, inplace=True)  
     weatherData = weatherData.reset_index(drop=True)
     
     # Filter only June, August and September 2017, which we have bike availability data from
-    weatherData = weatherData[weatherData.Month.isin([6, 8, 9])]
+    weatherData = weatherData[
+        ((weatherData.Year == 2017) & (weatherData.Month.isin([6, 8, 9]))) 
+        # ADD THE NEW MONTHS HERE AS OR-CONDITIONS
+        #| ((weatherData.Year == 2016) & (weatherData.Month.isin([7, 8, 9])))
+        ]
+    #print('Weatherdata filtered: ', weatherData)
 
     # Sort bikedata by date
     bikeData = bikeData.sort_values(by=['time', 'stationid'])
